718c70db5d0a5932b983af6987f5418a66a6a792
杨淮生 <codefan@centit.com>
2017-08-01 17:35:15 +0800

    cas 迁移到 最新的版本

--- centit-cas/src/main/java/com/centit/framework/cas/CentitAuthenticationHandler.java
+++ centit-cas/src/main/java/com/centit/framework/cas/CentitAuthenticationHandler.java
@@ -1,60 +1,33 @@
 package com.centit.framework.cas;
 
-import org.apereo.cas.adaptors.jdbc.AbstractJdbcUsernamePasswordAuthenticationHandler;
-import org.apereo.cas.authentication.*;
+import org.apereo.cas.adaptors.jdbc.QueryDatabaseAuthenticationHandler;
 import org.apereo.cas.authentication.principal.PrincipalFactory;
 import org.apereo.cas.services.ServicesManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import javax.security.auth.login.FailedLoginException;
 import javax.sql.DataSource;
-import java.security.GeneralSecurityException;
+import java.util.Map;
 
 @Component("centitAuthenticationHandler")
-public class CentitAuthenticationHandler extends AbstractJdbcUsernamePasswordAuthenticationHandler {
+public class CentitAuthenticationHandler extends QueryDatabaseAuthenticationHandler {
 
-    private static final String DEFAULT_PASSWORD_FIELD = "password";
-    private static final String DEFAULT_SALT_FIELD = "salt";
 
-    private BCryptPasswordEncoder passwordEncoder ;
 
-    /*@Autowired(required = false)
-    public CentitAuthenticationHandler(@Qualifier("queryEncodeDatabaseDataSource")
-                                       final DataSource datasource,
-                                       @Value("${cas.jdbc.authn.query.encode.sql:}")
-                                       final String sql) {
-        super("jdbc", (ServicesManager) null, (PrincipalFactory) null, 1,  datasource );
+    private static final Logger LOGGER = LoggerFactory.getLogger(CentitAuthenticationHandler.class);
 
-        this.sql = sql;
-    }*/
 
-    public CentitAuthenticationHandler(String name, ServicesManager servicesManager,
-                                       PrincipalFactory principalFactory, Integer order, DataSource dataSource) {
-        super(name, servicesManager, principalFactory, order, dataSource);
-        passwordEncoder = new BCryptPasswordEncoder(11);
+
+    public CentitAuthenticationHandler(final String name, final ServicesManager servicesManager,
+                                              final PrincipalFactory principalFactory,
+                                              final Integer order, final DataSource dataSource, final String sql,
+                                              final String fieldPassword, final String fieldExpired, final String fieldDisabled,
+                                              final Map<String, String> attributes) {
+        super(name, servicesManager, principalFactory, order, dataSource,
+                sql,fieldPassword,fieldExpired,fieldDisabled,attributes);
+
+       super.setPasswordEncoder(new BCryptPasswordEncoder(11));
     }
-
-
-    @Override
-    protected HandlerResult authenticateUsernamePasswordInternal(
-            UsernamePasswordCredential transformedCredential, String originalPassword)
-            throws GeneralSecurityException, PreventedException {
-        boolean matched = passwordEncoder.matches(originalPassword,transformedCredential.getPassword());
-        if(!matched)
-            throw new FailedLoginException("用户名 密码不匹配");
-
-        return new DefaultHandlerResult(this,  new BasicCredentialMetaData ( transformedCredential),
-                this.principalFactory.createPrincipal(transformedCredential.getId()));
-    }
-
-  /*  @Override
-    public boolean preAuthenticate(Credential credential) {
-        return true;
-    }
-
-    @Override
-    public HandlerResult postAuthenticate(Credential credential, HandlerResult result) {
-        return result;
-    }*/
 }
