424e2accc2375b44e29bd13cc61b7e669accce63
codefan <codefan@sina.com>
2017-12-04 17:23:27 +0800

    添加测试用例

--- centit-ip-app-demo/src/main/java/com/centit/framework/ip/app/demo/config/ServiceConfig.java
+++ centit-ip-app-demo/src/main/java/com/centit/framework/ip/app/demo/config/ServiceConfig.java
@@ -4,6 +4,7 @@ import com.centit.framework.components.impl.NotificationCenterImpl;
 import com.centit.framework.components.impl.TextOperationLogWriterImpl;
 import com.centit.framework.config.SpringSecurityCasConfig;
 import com.centit.framework.config.SpringSecurityDaoConfig;
+import com.centit.framework.ip.app.config.IPAppSystemBeanConfig;
 import com.centit.framework.ip.app.demo.listener.InstantiationServiceBeanPostProcessor;
 import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.model.adapter.OperationLogWriter;
@@ -13,7 +14,8 @@ import org.springframework.context.annotation.*;
  * Created by codefan on 17-7-18.
  */
 @Configuration
-@Import({SpringSecurityCasConfig.class,
+@Import({IPAppSystemBeanConfig.class,
+        SpringSecurityCasConfig.class,
         SpringSecurityDaoConfig.class})
 @ComponentScan(basePackages = "com.otherpackage",
         excludeFilters = @ComponentScan.Filter(value = org.springframework.stereotype.Controller.class))
