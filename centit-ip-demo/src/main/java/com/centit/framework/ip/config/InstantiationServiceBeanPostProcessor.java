6f5a09af31dd6e24660918e633f2dcc377ef8269
杨淮生 <codefan@centit.com>
2018-12-17 18:20:16 +0800

    修改 异常处理参数

--- centit-ip-demo/src/main/java/com/centit/framework/ip/config/InstantiationServiceBeanPostProcessor.java
+++ centit-ip-demo/src/main/java/com/centit/framework/ip/config/InstantiationServiceBeanPostProcessor.java
@@ -2,11 +2,13 @@ package com.centit.framework.ip.config;
 
 import com.centit.framework.components.CodeRepositoryCache;
 import com.centit.framework.components.OperationLogCenter;
+import com.centit.framework.core.controller.BaseController;
 import com.centit.framework.model.adapter.MessageSender;
 import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.model.adapter.OperationLogWriter;
 import com.centit.framework.model.adapter.PlatformEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
@@ -27,10 +29,16 @@ public class InstantiationServiceBeanPostProcessor implements ApplicationListene
     @Autowired
     private PlatformEnvironment platformEnvironment;
 
+    @Value("${login.nouser.exception.ashttperror:true}")
+    protected boolean loginUserNotLoginExceptionAshHttpError;
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event)
     {
         CodeRepositoryCache.setPlatformEnvironment(platformEnvironment);
+        CodeRepositoryCache.setAllCacheFreshPeriod(CodeRepositoryCache.CACHE_KEEP_FRESH);
+
+        BaseController.setLoginUserNotLoginExceptionAshHttpError(loginUserNotLoginExceptionAshHttpError);
 
         if(innerMessageManager!=null) {
             notificationCenter.registerMessageSender("innerMsg", innerMessageManager);
