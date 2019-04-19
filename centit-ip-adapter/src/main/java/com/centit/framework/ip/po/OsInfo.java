d2c79c04bd0b17f9be48a8342ba85b8d1f468a99
杨淮生 <codefan@centit.com>
2019-04-19 15:43:04 +0800

    新建2.0-SNAPSHOT版本

--- centit-ip-adapter/src/main/java/com/centit/framework/ip/po/OsInfo.java
+++ centit-ip-adapter/src/main/java/com/centit/framework/ip/po/OsInfo.java
@@ -1,20 +1,21 @@
 package com.centit.framework.ip.po;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class OsInfo implements java.io.Serializable {
     private static final long serialVersionUID = 1L;
 
     private String osId;
     private String osName;
     private String osUrl;
-    private String ddeSyncUrl;
-    private String sysDataPushOption;
     private String created;
     private Date lastModifyDate;
     private Date createTime;
-    private String loginUserName;
-    private String loginUserPassword;
+    /**oauth2 登录密码 密文*/
+    private String oauthPassword;
     // Constructors
 
     /**
@@ -37,110 +38,20 @@ public class OsInfo implements java.io.Serializable {
             String created, Date lastUpdateTime, Date createTime) {
         this.osId = osId;
         this.osName = osName;
-        this.ddeSyncUrl=ddeSyncUrl;
-        this.sysDataPushOption=sysDataPushOption;
         this.osUrl = osUrl;
         this.created = created;
         this.lastModifyDate= lastUpdateTime;
         this.createTime = createTime;
     }
 
-
-
-
-    public String getOsId() {
-        return this.osId;
-    }
-
-    public void setOsId(String osId) {
-        this.osId = osId;
-    }
-    // Property accessors
-
-    public String getOsName() {
-        return this.osName;
-    }
-
-    public void setOsName(String osName) {
-        this.osName = osName;
-    }
-
-    public String getCreated() {
-        return this.created;
-    }
-
-    public void setCreated(String created) {
-        this.created = created;
-    }
-
-
-    public String getOsUrl() {
-		return osUrl;
-    }
-
-    public void setOsUrl(String osUrl) {
-		this.osUrl = osUrl;
-    }
-
-    public String getDdeSyncUrl() {
-		return ddeSyncUrl;
-    }
-
-    public void setDdeSyncUrl(String ddeSyncUrl) {
-		this.ddeSyncUrl = ddeSyncUrl;
-    }
-
-    public String getSysDataPushOption() {
-		return sysDataPushOption;
-    }
-
-    public void setSysDataPushOption(String sysDataPushOption) {
-		this.sysDataPushOption = sysDataPushOption;
-    }
-
-    public Date getLastModifyDate() {
-		return lastModifyDate;
-    }
-
-    public void setLastModifyDate(Date lastModifyDate) {
-		this.lastModifyDate = lastModifyDate;
-    }
-
-    public Date getCreateTime() {
-        return this.createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getLoginUserName() {
-        return loginUserName;
-    }
-
-    public void setLoginUserName(String loginUserName) {
-        this.loginUserName = loginUserName;
-    }
-
-    public String getLoginUserPassword() {
-        return loginUserPassword;
-    }
-
-    public void setLoginUserPassword(String loginUserPassword) {
-        this.loginUserPassword = loginUserPassword;
-    }
-
     public void copy(OsInfo other) {
         this.setOsId(other.getOsId());
         this.osName = other.getOsName();
-        this.ddeSyncUrl=other.getDdeSyncUrl();
-        this.sysDataPushOption=other.getSysDataPushOption();
         this.osUrl = other.getOsUrl();
         this.lastModifyDate= other.getLastModifyDate();
         this.createTime = other.getCreateTime();
         this.created = other.getCreated();
-        this.loginUserName = other.getLoginUserName();
-        this.loginUserPassword = other.getLoginUserPassword();
+        this.oauthPassword = other.getOauthPassword();
     }
 
     public void copyNotNullProperty(OsInfo other) {
@@ -148,10 +59,6 @@ public class OsInfo implements java.io.Serializable {
             this.setOsId(other.getOsId());
         if (other.getOsName() != null)
             this.osName = other.getOsName();
-        if (other.getDdeSyncUrl() != null)
-            this.ddeSyncUrl=other.getDdeSyncUrl();
-        if (other.getSysDataPushOption() != null)
-            this.sysDataPushOption=other.getSysDataPushOption();
         if (other.getOsUrl() != null)
             this.osUrl = other.getOsUrl();
         if (other.getLastModifyDate() != null)
@@ -160,22 +67,17 @@ public class OsInfo implements java.io.Serializable {
             this.created = other.getCreated();
         if (other.getCreateTime() != null)
             this.createTime = other.getCreateTime();
-        if (other.getLoginUserName() != null)
-            this.loginUserName = other.getLoginUserName();
-        if (other.getLoginUserPassword() != null)
-            this.loginUserPassword = other.getLoginUserPassword();
+        if (other.getOauthPassword() != null)
+            this.oauthPassword = other.getOauthPassword();
     }
 
     public void clearProperties() {
         this.osId = null;
         this.osName = null;
-        this.ddeSyncUrl=null;
-        this.sysDataPushOption=null;
         this.osUrl = null;
         this.created = null;
         this.lastModifyDate= null;
         this.createTime = null;
-        this.loginUserName = null;
-        this.loginUserPassword = null;
+        this.oauthPassword = null;
     }
 }
--- centit-ip-module/src/main/java/com/centit/framework/ip/po/OsInfo.java
+++ centit-ip-module/src/main/java/com/centit/framework/ip/po/OsInfo.java
@@ -7,6 +7,7 @@ import com.centit.support.database.orm.GeneratorType;
 import com.centit.support.database.orm.ValueGenerator;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Column;
@@ -18,6 +19,7 @@ import java.util.Date;
 @Entity
 @Table(name = "F_OS_INFO")
 @ApiModel(value="系统信息对象",description="系统信息对象 OsInfo")
+@Data
 public class OsInfo implements java.io.Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -38,23 +40,10 @@ public class OsInfo implements java.io.Serializable {
     @ApiModelProperty(value = "系统地址",name = "osUrl")
     private String osUrl;
 
-    @Column(name = "DDE_SYNC_URL")
-    @Length(max = 200, message = "字段长度不能大于{max}")
-    private String ddeSyncUrl;
-
-    @Column(name = "SYS_DATA_PUSH_OPTION")
-    @Length(max = 32, message = "字段长度不能大于{max}")
-    private String sysDataPushOption;
-
-    @Column(name = "LOGIN_USER_NAME")
-    @Length(max = 64, message = "字段长度不能大于{max}")
-    @ApiModelProperty(value = "登录名",name = "loginUserName")
-    private String loginUserName;
-
-    @Column(name = "LOGIN_USER_PASSWORD")
-    @Length(max = 64, message = "字段长度不能大于{max}")
-    @ApiModelProperty(value = "登录密码",name = "loginUserPassword")
-    private String loginUserPassword;
+    @Column(name = "OAUTH_PASSWORD")
+    @Length(max = 128, message = "字段长度不能大于{max}")
+    @ApiModelProperty(value = "oauth2 登录密码 密文",name = "oauthPassword")
+    private String oauthPassword;
 
     @Column(name = "CREATED")
     @Length(max = 8, message = "字段长度不能大于{max}")
@@ -90,107 +79,20 @@ public class OsInfo implements java.io.Serializable {
             String created, Date lastUpdateTime, Date createTime) {
         this.osId = osId;
         this.osName = osName;
-        this.ddeSyncUrl=ddeSyncUrl;
-        this.sysDataPushOption=sysDataPushOption;
         this.osUrl = osUrl;
         this.created = created;
         this.lastModifyDate= lastUpdateTime;
         this.createTime = createTime;
     }
 
-    public String getOsId() {
-        return this.osId;
-    }
-
-    public void setOsId(String osId) {
-        this.osId = osId;
-    }
-    // Property accessors
-
-    public String getOsName() {
-        return this.osName;
-    }
-
-    public void setOsName(String osName) {
-        this.osName = osName;
-    }
-
-    public String getCreated() {
-        return this.created;
-    }
-
-    public void setCreated(String created) {
-        this.created = created;
-    }
-
-    public String getOsUrl() {
-		return osUrl;
-    }
-
-    public void setOsUrl(String osUrl) {
-		this.osUrl = osUrl;
-    }
-
-    public String getDdeSyncUrl() {
-		return ddeSyncUrl;
-    }
-
-    public void setDdeSyncUrl(String ddeSyncUrl) {
-		this.ddeSyncUrl = ddeSyncUrl;
-    }
-
-    public String getSysDataPushOption() {
-		return sysDataPushOption;
-    }
-
-    public void setSysDataPushOption(String sysDataPushOption) {
-		this.sysDataPushOption = sysDataPushOption;
-    }
-
-    public String getLoginUserName() {
-        return loginUserName;
-    }
-
-    public void setLoginUserName(String loginUserName) {
-        this.loginUserName = loginUserName;
-    }
-
-    public String getLoginUserPassword() {
-        return loginUserPassword;
-    }
-
-    public void setLoginUserPassword(String loginUserPassword) {
-        this.loginUserPassword = loginUserPassword;
-    }
-
-    public Date getLastModifyDate() {
-		return lastModifyDate;
-    }
-
-    public void setLastModifyDate(Date lastModifyDate) {
-		this.lastModifyDate = lastModifyDate;
-    }
-
-    public Date getCreateTime() {
-        return this.createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-
     public void copy(OsInfo other) {
         this.setOsId(other.getOsId());
         this.osName = other.getOsName();
-        this.ddeSyncUrl=other.getDdeSyncUrl();
-        this.sysDataPushOption=other.getSysDataPushOption();
         this.osUrl = other.getOsUrl();
         this.lastModifyDate= other.getLastModifyDate();
         this.createTime = other.getCreateTime();
         this.created = other.getCreated();
-        this.loginUserName = other.getLoginUserName();
-        this.loginUserPassword = other.getLoginUserPassword();
+        this.oauthPassword = other.getOauthPassword();
     }
 
     public void copyNotNullProperty(OsInfo other) {
@@ -198,10 +100,6 @@ public class OsInfo implements java.io.Serializable {
             this.setOsId(other.getOsId());
         if (other.getOsName() != null)
             this.osName = other.getOsName();
-        if (other.getDdeSyncUrl() != null)
-            this.ddeSyncUrl=other.getDdeSyncUrl();
-        if (other.getSysDataPushOption() != null)
-            this.sysDataPushOption=other.getSysDataPushOption();
         if (other.getOsUrl() != null)
             this.osUrl = other.getOsUrl();
         if (other.getLastModifyDate() != null)
@@ -210,22 +108,17 @@ public class OsInfo implements java.io.Serializable {
             this.created = other.getCreated();
         if (other.getCreateTime() != null)
             this.createTime = other.getCreateTime();
-        if (other.getLoginUserName() != null)
-            this.loginUserName = other.getLoginUserName();
-        if (other.getLoginUserPassword() != null)
-            this.loginUserPassword = other.getLoginUserPassword();
+        if (other.getOauthPassword() != null)
+            this.oauthPassword = other.getOauthPassword();
     }
 
     public void clearProperties() {
         this.osId = null;
         this.osName = null;
-        this.ddeSyncUrl=null;
-        this.sysDataPushOption=null;
         this.osUrl = null;
         this.created = null;
         this.lastModifyDate= null;
         this.createTime = null;
-        this.loginUserName = null;
-        this.loginUserPassword = null;
+        this.oauthPassword = null;
     }
 }
