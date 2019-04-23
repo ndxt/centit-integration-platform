b44fd1d650f885e02ab57c0261a8a8132a5f14a1
杨淮生 <codefan@centit.com>
2019-04-23 10:52:21 +0800

    添加spring boot 相关的配置类

---
+++ centit-ip-app-boot-config/src/main/java/com/centit/framework/ip/app/config/FrameworkBeanConfiguation.java
@@ -0,0 +1,132 @@
+package com.centit.framework.ip.app.config;
+
+import com.alibaba.fastjson.parser.Feature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.centit.framework.ip.app.service.impl.IPClientIntegrationEnvironment;
+import com.centit.framework.ip.app.service.impl.IPClientPlatformEnvironment;
+import com.centit.framework.ip.app.service.impl.IntegrationEnvironmentProxy;
+import com.centit.framework.ip.app.service.impl.PlatformEnvironmentProxy;
+import com.centit.framework.ip.service.IntegrationEnvironment;
+import com.centit.framework.ip.service.impl.JsonIntegrationEnvironment;
+import com.centit.framework.model.adapter.PlatformEnvironment;
+import com.centit.framework.security.model.CentitPasswordEncoder;
+import com.centit.framework.security.model.CentitUserDetailsService;
+import com.centit.framework.security.model.StandardPasswordEncoderImpl;
+import com.centit.framework.staticsystem.service.impl.JsonPlatformEnvironment;
+import com.centit.framework.staticsystem.service.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.MediaType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EnableConfigurationProperties(FrameworkProperties.class)
+@Configuration("environmentConfiguation")
+public class FrameworkBeanConfiguation {
+
+    @Autowired
+    private FrameworkProperties frameworkProperties;
+
+    @Bean
+    public FastJsonHttpMessageConverter fastJsonHttpMessageConverter(){
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter =
+                new FastJsonHttpMessageConverter();
+        List<MediaType> supportedMediaTypes = new ArrayList<>();
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+
+        fastJsonHttpMessageConverter.setSupportedMediaTypes(supportedMediaTypes);
+
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setFeatures(Feature.AllowArbitraryCommas,Feature.AllowUnQuotedFieldNames,
+                Feature.DisableCircularReferenceDetect);
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+        return fastJsonHttpMessageConverter;
+    }
+
+    @Bean({"passwordEncoder"})
+    public CentitPasswordEncoder passwordEncoder() {
+        return new StandardPasswordEncoderImpl();
+    }
+
+    @Bean
+    @Lazy(value = false)
+    public PlatformEnvironment platformEnvironment(
+            @Autowired CentitPasswordEncoder passwordEncoder) {
+
+        boolean ipEnable = frameworkProperties.getIp().isEnable();
+
+        JsonPlatformEnvironment jsonPlatformEnvironment = new JsonPlatformEnvironment();
+        jsonPlatformEnvironment.setAppHome(frameworkProperties.getApp().getHome());
+        jsonPlatformEnvironment.setPasswordEncoder(passwordEncoder);
+        if(!ipEnable) {
+            return jsonPlatformEnvironment;
+        }
+
+        IPClientPlatformEnvironment ipPlatformEnvironment = new IPClientPlatformEnvironment();
+        ipPlatformEnvironment.setTopOptId(frameworkProperties.getIp().getTopoptid());
+        ipPlatformEnvironment.createPlatAppSession(
+                frameworkProperties.getIp().getHome(),
+                frameworkProperties.getIp().isAuthEnable(),
+                frameworkProperties.getIp().getUsercode(),
+                frameworkProperties.getIp().getPassword());
+
+        List<PlatformEnvironment> evrnMangers = new ArrayList<>();
+        evrnMangers.add(ipPlatformEnvironment);
+        evrnMangers.add(jsonPlatformEnvironment);
+
+        PlatformEnvironmentProxy platformEnvironment = new PlatformEnvironmentProxy();
+        platformEnvironment.setEvrnMangers(evrnMangers);
+
+        return platformEnvironment;
+    }
+
+    @Bean
+    @Lazy(value = false)
+    public IntegrationEnvironment integrationEnvironment() {
+
+        boolean ipEnable = frameworkProperties.getIp().isEnable();
+
+        JsonIntegrationEnvironment staticIntegrationEnvironment = new JsonIntegrationEnvironment();
+        staticIntegrationEnvironment.setAppHome(frameworkProperties.getApp().getHome());
+
+        staticIntegrationEnvironment.reloadIPEnvironmen();
+        if(!ipEnable) {
+            return staticIntegrationEnvironment;
+        }
+
+
+        IPClientIntegrationEnvironment ipIntegrationEnvironment = new IPClientIntegrationEnvironment();
+        ipIntegrationEnvironment.createPlatAppSession(
+                frameworkProperties.getIp().getHome(),
+                frameworkProperties.getIp().isAuthEnable(),
+                frameworkProperties.getIp().getUsercode(),
+                frameworkProperties.getIp().getPassword());
+        //ipPlatformEnvironment.init();
+
+        List<IntegrationEnvironment> evrnMangers = new ArrayList<>();
+        evrnMangers.add(ipIntegrationEnvironment);
+        evrnMangers.add(staticIntegrationEnvironment);
+
+        IntegrationEnvironmentProxy integrationEnvironment = new IntegrationEnvironmentProxy();
+        integrationEnvironment.setEvrnMangers(evrnMangers);
+
+        return integrationEnvironment;
+    }
+
+    @Bean
+    public CentitUserDetailsService centitUserDetailsService(@Autowired PlatformEnvironment platformEnvironment) {
+        UserDetailsServiceImpl userDetailsService = new UserDetailsServiceImpl();
+        userDetailsService.setPlatformEnvironment(platformEnvironment);
+        return userDetailsService;
+    }
+
+
+}
---
+++ centit-ip-app-boot-config/src/main/java/com/centit/framework/ip/app/config/FrameworkProperties.java
@@ -0,0 +1,29 @@
+package com.centit.framework.ip.app.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(FrameworkProperties.PREFIX)
+@Data
+public class FrameworkProperties {
+    public static final String PREFIX = "framework";
+
+    private AppConfig app;
+    private IpConfig ip;
+
+    @Data
+    public static class AppConfig{
+        private String home;
+    }
+
+    @Data
+    public static class IpConfig{
+        private boolean enable;
+        private String home;
+        private String topoptid;
+        private boolean authEnable;
+        private String usercode;
+        private String password;
+    }
+
+}
