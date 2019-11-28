fbee8ba2c1e389d0a40ec49723fde42b7135c79b
codefan <codefan@sina.com>
2019-11-28 08:48:23 +0800

    F_OS_INFO 添加三个字段 REL_OPT_ID, OS_HOME_PAGE, OS_AUTH_USER

--- centit-ip-module/src/main/java/com/centit/framework/ip/po/OsInfo.java
+++ centit-ip-module/src/main/java/com/centit/framework/ip/po/OsInfo.java
@@ -2,7 +2,6 @@ package com.centit.framework.ip.po;
 
 import com.centit.framework.core.dao.DictionaryMap;
 import com.centit.support.database.orm.GeneratorCondition;
-import com.centit.support.database.orm.GeneratorTime;
 import com.centit.support.database.orm.GeneratorType;
 import com.centit.support.database.orm.ValueGenerator;
 import io.swagger.annotations.ApiModel;
@@ -25,8 +24,7 @@ public class OsInfo implements java.io.Serializable {
 
     @Id
     @Column(name = "OS_ID")
-//    @GeneratedValue(generator = "assignedGenerator")
-//    @GenericGenerator(name = "assignedGenerator", strategy = "assigned")
+    @ValueGenerator(strategy = GeneratorType.UUID22)
     @ApiModelProperty(value = "业务系统ID",name = "osId",required = true)
     private String osId;
 
@@ -35,22 +33,45 @@ public class OsInfo implements java.io.Serializable {
     @ApiModelProperty(value = "系统名",name = "osName")
     private String osName;
 
+    /**
+     * 业务系统后台url
+     */
     @Column(name = "OS_URL")
     @Length(max = 200, message = "字段长度不能大于{max}")
     @ApiModelProperty(value = "系统地址",name = "osUrl")
     private String osUrl;
+
+    /**
+     * 业务系统首页
+     */
+    @Column(name = "OS_HOME_PAGE")
+    @Length(max = 300, message = "字段长度不能大于{max}")
+    @ApiModelProperty(value = "系统地址",name = "osHomePage")
+    private String osHomePage;
+
+    @Column(name = "OAUTH_USER")
+    @Length(max = 32, message = "字段长度不能大于{max}")
+    @ApiModelProperty(value = "oauth2 登录用户名",name = "oauthUser")
+    private String oauthUser;
 
     @Column(name = "OAUTH_PASSWORD")
     @Length(max = 128, message = "字段长度不能大于{max}")
     @ApiModelProperty(value = "oauth2 登录密码 密文",name = "oauthPassword")
     private String oauthPassword;
 
+    /**
+     * 关联的顶层菜单ID
+     */
+    @Column(name = "REL_OPT_ID")
+    @Length(max = 32, message = "字段长度不能大于{max}")
+    private String relOptId; // 顶层业务编号
+
     @Column(name = "CREATED")
     @Length(max = 8, message = "字段长度不能大于{max}")
     @DictionaryMap(fieldName = "createUserName", value = "userCode")
     private String created;
 
-    @ValueGenerator( strategy= GeneratorType.FUNCTION, value = "today()", condition = GeneratorCondition.ALWAYS, occasion = GeneratorTime.ALWAYS )
+    @ValueGenerator( strategy= GeneratorType.FUNCTION, value = "today()", condition = GeneratorCondition.ALWAYS )
     @Column(name = "LAST_MODIFY_DATE")
     private Date lastModifyDate;
 
@@ -65,60 +86,9 @@ public class OsInfo implements java.io.Serializable {
     public OsInfo() {
     }
 
-    public OsInfo(
-            String osId
-            , String osName) {
+    public OsInfo(String osId, String osName) {
         this.osId = osId;
         this.osName = osName;
     }
 
-    public OsInfo(
-            String osId
-            , String osName,
-            String sysDataPushOption, String osUrl,String ddeSyncUrl,
-            String created, Date lastUpdateTime, Date createTime) {
-        this.osId = osId;
-        this.osName = osName;
-        this.osUrl = osUrl;
-        this.created = created;
-        this.lastModifyDate= lastUpdateTime;
-        this.createTime = createTime;
-    }
-
-    public void copy(OsInfo other) {
-        this.setOsId(other.getOsId());
-        this.osName = other.getOsName();
-        this.osUrl = other.getOsUrl();
-        this.lastModifyDate= other.getLastModifyDate();
-        this.createTime = other.getCreateTime();
-        this.created = other.getCreated();
-        this.oauthPassword = other.getOauthPassword();
-    }
-
-    public void copyNotNullProperty(OsInfo other) {
-        if (other.getOsId() != null)
-            this.setOsId(other.getOsId());
-        if (other.getOsName() != null)
-            this.osName = other.getOsName();
-        if (other.getOsUrl() != null)
-            this.osUrl = other.getOsUrl();
-        if (other.getLastModifyDate() != null)
-            this.lastModifyDate= other.getLastModifyDate();
-        if (other.getCreated() != null)
-            this.created = other.getCreated();
-        if (other.getCreateTime() != null)
-            this.createTime = other.getCreateTime();
-        if (other.getOauthPassword() != null)
-            this.oauthPassword = other.getOauthPassword();
-    }
-
-    public void clearProperties() {
-        this.osId = null;
-        this.osName = null;
-        this.osUrl = null;
-        this.created = null;
-        this.lastModifyDate= null;
-        this.createTime = null;
-        this.oauthPassword = null;
-    }
 }
