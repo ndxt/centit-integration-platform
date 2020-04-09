9740409d61e21814204976c15e354833c967f94b
zhf <zhf@centit.com>
2020-04-09 16:44:37 +0800

    添加查询条件

--- centit-ip-module/src/main/java/com/centit/framework/ip/dao/DatabaseInfoDao.java
+++ centit-ip-module/src/main/java/com/centit/framework/ip/dao/DatabaseInfoDao.java
@@ -17,31 +17,30 @@ import java.util.List;
 import java.util.Map;
 
 @Repository
-public class DatabaseInfoDao extends BaseDaoImpl<DatabaseInfo,String> {
+public class DatabaseInfoDao extends BaseDaoImpl<DatabaseInfo, String> {
 
+    @Override
     public Map<String, String> getFilterField() {
-        if (filterField == null) {
-            filterField = new HashMap<>();
-            filterField.put("databaseName", CodeBook.LIKE_HQL_ID);
-            filterField.put("databaseCode", CodeBook.EQUAL_HQL_ID);
-            filterField.put("osId", CodeBook.EQUAL_HQL_ID);
-            filterField.put("databaseType", CodeBook.LIKE_HQL_ID);
-            filterField.put("hostPort", CodeBook.LIKE_HQL_ID);
-            filterField.put("databaseUrl", CodeBook.LIKE_HQL_ID);
-            filterField.put("username", CodeBook.LIKE_HQL_ID);
-            filterField.put("password", CodeBook.LIKE_HQL_ID);
-            filterField.put("dataDesc", CodeBook.LIKE_HQL_ID);
-            filterField.put("createTime", CodeBook.LIKE_HQL_ID);
-            filterField.put("created", CodeBook.LIKE_HQL_ID);
-        }
+        Map<String, String> filterField = new HashMap<>();
+        filterField.put("databaseName", CodeBook.LIKE_HQL_ID);
+        filterField.put("databaseCode", CodeBook.EQUAL_HQL_ID);
+        filterField.put("osId", CodeBook.EQUAL_HQL_ID);
+        filterField.put("databaseType", CodeBook.LIKE_HQL_ID);
+        filterField.put("hostPort", CodeBook.LIKE_HQL_ID);
+        filterField.put("databaseUrl", CodeBook.LIKE_HQL_ID);
+        filterField.put("username", CodeBook.LIKE_HQL_ID);
+        filterField.put("password", CodeBook.LIKE_HQL_ID);
+        filterField.put("dataDesc", CodeBook.LIKE_HQL_ID);
+        filterField.put("createTime", CodeBook.LIKE_HQL_ID);
+        filterField.put("created", CodeBook.LIKE_HQL_ID);
         return filterField;
     }
 
     public boolean connectionTest(DatabaseInfo databaseInfo) {
         return DataSourceDescription.testConntect(new DataSourceDescription(
-                databaseInfo.getDatabaseUrl(),
-                databaseInfo.getUsername(),
-                databaseInfo.getPassword()));
+            databaseInfo.getDatabaseUrl(),
+            databaseInfo.getUsername(),
+            databaseInfo.getPassword()));
     }
 
     public List<DatabaseInfo> listDatabase() {
@@ -49,23 +48,23 @@ public class DatabaseInfoDao extends BaseDaoImpl<DatabaseInfo,String> {
     }
 
     public DatabaseInfo getDatabaseInfoById(String databaseCode) {
-        DatabaseInfo dbi=this.getObjectById(databaseCode);
+        DatabaseInfo dbi = this.getObjectById(databaseCode);
         return dbi;
     }
 
     //jdbc
     public String getNextKey() {
         return StringBaseOpt.fillZeroForString(
-                String.valueOf(
-                    JdbcTemplateUtils.getSequenceNextValue(
-                        this.jdbcTemplate, "S_DATABASECODE")), 10);
+            String.valueOf(
+                JdbcTemplateUtils.getSequenceNextValue(
+                    this.jdbcTemplate, "S_DATABASECODE")), 10);
     }
 
-    public JSONArray queryDatabaseAsJson(String databaseName, PageDesc pageDesc){
-        if(StringUtils.isBlank(databaseName)){
+    public JSONArray queryDatabaseAsJson(String databaseName, PageDesc pageDesc) {
+        if (StringUtils.isBlank(databaseName)) {
             return super.listObjectsAsJson(new HashMap<>(1), pageDesc);
         }
         String matchStr = QueryUtils.getMatchString(databaseName);
-        return super.listObjectsByFilterAsJson("where DATABASE_NAME like ? or DATABASE_URL like ?", new Object[]{matchStr,matchStr},pageDesc);
+        return super.listObjectsByFilterAsJson("where DATABASE_NAME like ? or DATABASE_URL like ?", new Object[]{matchStr, matchStr}, pageDesc);
     }
 }
