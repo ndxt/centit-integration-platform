65d0b5585ab2a963c37426119cdfb3d7e06ef1a3
tian_y <tian_y@centit.com>
2022-02-23 15:07:30 +0800

    LADP登录接口改成POST请求

--- centit-ip-users-register/src/main/java/com/centit/framework/users/controller/LdapLogin.java
+++ centit-ip-users-register/src/main/java/com/centit/framework/users/controller/LdapLogin.java
@@ -15,7 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -49,7 +49,7 @@ public class LdapLogin {
     private UserSyncDirectoryManager userSyncDirectoryManager;
 
     @ApiOperation(value = "ldap登录", notes = "ldap登录")
-    @GetMapping(value = "/login")
+    @PostMapping(value = "/login")
     public String login(@RequestParam("loginName") String loginName,
                         @RequestParam("passWord") String passWord,
                         @RequestParam("returnUrl") String returnUrl,
