02ab20e4fbf0ac0b4d918852a339307e42b0ea9d
周抄 <zhou_c@centit.com>
2022-08-29 14:28:29 +0800

    修改日志实现(发布测试)

--- centit-ip-demo/src/main/java/com/centit/framework/ip/config/InstantiationServiceBeanPostProcessor.java
+++ centit-ip-demo/src/main/java/com/centit/framework/ip/config/InstantiationServiceBeanPostProcessor.java
@@ -5,12 +5,14 @@ import com.centit.framework.components.CodeRepositoryCache;
 import com.centit.framework.components.OperationLogCenter;
 import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.model.adapter.PlatformEnvironment;
-import com.centit.framework.system.service.OptLogManager;
+import com.centit.framework.system.service.OperationLogManager;
 import com.centit.framework.system.service.impl.DBPlatformEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+
+import javax.annotation.Resource;
 
 /**
  * Created by codefan on 17-7-6.
@@ -20,8 +22,8 @@ public class InstantiationServiceBeanPostProcessor implements ApplicationListene
     @Autowired
     protected NotificationCenter notificationCenter;
 
-    @Autowired(required = false)
-    private OptLogManager optLogManager;
+    @Resource
+    private OperationLogManager operationLogManager;
 
     @Autowired
     private PlatformEnvironment platformEnvironment;
@@ -40,8 +42,8 @@ public class InstantiationServiceBeanPostProcessor implements ApplicationListene
         CodeRepositoryCache.setAllCacheFreshPeriod(CodeRepositoryCache.CACHE_FRESH_PERIOD_SECONDS);
         WebOptUtils.setExceptionNotAsHttpError(httpExceptionNotAsHttpError);
         WebOptUtils.setIsTenant(supportTenant);
-        if (optLogManager != null) {
-            OperationLogCenter.registerOperationLogWriter(optLogManager);
+        if (operationLogManager != null) {
+            OperationLogCenter.registerOperationLogWriter(operationLogManager);
         }
         DBPlatformEnvironment dbPlatformEnvironment = event.getApplicationContext().getBean("dbPlatformEnvironment",
             DBPlatformEnvironment.class);
