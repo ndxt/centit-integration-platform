305260b4f0845350416e912e8f3ab7d44936d5ec
zfg <zfg@centit.com>
2022-12-09 15:03:00 +0800

    第三方登录添加统一门户单点登录

---
+++ centit-ip-users-register/src/main/java/com/centit/framework/users/config/UniteConfig.java
@@ -0,0 +1,25 @@
+package com.centit.framework.users.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zfg
+ */
+@Configuration
+@Data
+public class UniteConfig {
+
+    @Value("${appId:}")
+    private String appId;
+
+    @Value("${loginCheckUrl:}")
+    private String loginCheckUrl;
+
+    @Value("${loginCheckExtendUrl:}")
+    private String loginCheckExtendUrl;
+
+    @Value("${uniteAppSecret:}")
+    private String uniteAppSecret;
+}
