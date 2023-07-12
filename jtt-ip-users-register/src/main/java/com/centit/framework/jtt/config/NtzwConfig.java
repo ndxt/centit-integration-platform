359d2de9d4cd95b8c20b1176dc21c3f80810879c
zfg <zfg@centit.com>
2023-07-12 16:28:44 +0800

    配置调整

--- jtt-ip-users-register/src/main/java/com/centit/framework/jtt/config/NtzwConfig.java
+++ jtt-ip-users-register/src/main/java/com/centit/framework/jtt/config/NtzwConfig.java
@@ -28,6 +28,6 @@ public class NtzwConfig {
     @Value("${ntSSL:true}")
     private String ntSSL;
 
-    @Value("${ntReturnUrl:https://cloud.centit.com/locode/page/A/apps/X9qvYWgSTR-O4nQSwxxW2g/pages/3vM_wWigQQmVbmm54JsgGw}")
+    @Value("${ntReturnUrl:https://cloud.centit.com/locode/page/A    /apps/X9qvYWgSTR-O4nQSwxxW2g}")
     private String ntReturnUrl;
 }
