47af0f4545307c31e66a779db0178ac81dc4ca0e
codefan <codefan@centit.com>
2023-12-25 09:04:00 +0800

    完善代码

--- centit-ip-tenant-manage/src/main/java/com/centit/framework/tenant/service/impl/TenantManageServiceImpl.java
+++ centit-ip-tenant-manage/src/main/java/com/centit/framework/tenant/service/impl/TenantManageServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
-@Service
+@Service("tenantManageServiceImpl")
 public class TenantManageServiceImpl implements TenantManageService {
     @Autowired
     private TenantInfoDao tenantInfoDao;
